7313126092e0505db0094a9b352737bb966940e5
Miguel Cobian <miguelcobian99@gmail.com>
2023-04-23 21:01:54 -0600

    Prototype para llenar las opciones del Select de Año

--- js/app.js
+++ js/app.js
@@ -5,4 +5,25 @@ function Seguro (marca, year, tipo) {
     this.marca = marca;
     this.year  = year;
     this.tipo  = tipo;
-}
+};
+
+function UI() {};
+
+//Llena las opciones de los años
+UI.prototype.llenarOpciones = () => {
+    const max = new Date().getFullYear(),
+          min = max - 20;
+    
+    const selectYear = document.querySelector('#year');
+
+    for(let i = max; i > min; i--){
+        let option = document.createElement('option');
+        option.value = i;
+        option.textContent = i;
+        selectYear.appendChild(option);
+    }
+};
+
+document.addEventListener('DOMContentLoaded', () =>{
+
+});
